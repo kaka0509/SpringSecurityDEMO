0748ddaf289b8ce3f891b19db4d849b3c3e4acb8
kaka0509 <fly2695103@163.com>
2019-07-08 21:31:27 +0800

    OAuth2认证后用户绑定、解绑

--- imooc-security-core/src/main/java/com/imooc/security/core/social/weixin/config/WeixinAutoConfig.java
+++ imooc-security-core/src/main/java/com/imooc/security/core/social/weixin/config/WeixinAutoConfig.java
@@ -2,12 +2,16 @@ package com.imooc.security.core.social.weixin.config;
 
 import com.imooc.security.core.properties.SecurityProperties;
 import com.imooc.security.core.properties.WeixinProperties;
+import com.imooc.security.core.social.ImoocConnectView;
 import com.imooc.security.core.social.weixin.connect.WeixinConnectionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.social.SocialAutoConfigurerAdapter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.social.connect.ConnectionFactory;
+import org.springframework.web.servlet.View;
 
 /**
  * @author Skye
@@ -15,7 +19,7 @@ import org.springframework.social.connect.ConnectionFactory;
  * imooc.security.social.qq.app-id 的值存在的情况下，下面的配置才会生效
  */
 @Configuration
-@ConditionalOnProperty(prefix = "imooc.security.social.weixin",name = "app-id")
+@ConditionalOnProperty(prefix = "imooc.security.social.weixin", name = "app-id")
 public class WeixinAutoConfig extends SocialAutoConfigurerAdapter {
 
     @Autowired
@@ -26,5 +30,17 @@ public class WeixinAutoConfig extends SocialAutoConfigurerAdapter {
         WeixinProperties weixinConfig = securityProperties.getSocial().getWeixin();
         return new WeixinConnectionFactory(weixinConfig.getProviderId(), weixinConfig.getAppId(),
                 weixinConfig.getAppSecret());
+    }
+
+    /**
+     * 当spring容器中存在名字为weixinConnectedView的bean则下面配置不生效
+     * connect/weixinConnect 解绑视图
+     * connect/weixinConnected 绑定视图
+     * @return
+     */
+    @Bean({"connect/weixinConnect", "connect/weixinConnected"})
+    @ConditionalOnMissingBean(name = "weixinConnectedView")
+    public View weixinConnectedView() {
+        return new ImoocConnectView();
     }
 }
